6acee9a9d58897cfcfd49c9eab519eb3e58cb5c3
johnlfield <johnlfield@users.noreply.github.com>
2020-09-15 03:34:03 -0600

    Initial commit

---
+++ constants.py
@@ -0,0 +1,16 @@
+#!/bin/python
+
+"""A centralized place to store conversion factors and other constants.
+"""
+
+C_concentration = 0.45
+N_to_N2O = 44.013/28.014
+C_to_CO2 = 44.009/12.011
+N2O_GWP100 = 298.0
+CH4_GWP100 = 25.0
+N2O_GWP100_AR5 = 265.0   # from WG1AR5_Chapter08_FINAL.pdf, Appendix 8.A
+CH4_GWP100_AR5 = 28.0   # from WG1AR5_Chapter08_FINAL.pdf, Appendix 8.A
+g_m2_to_kg_ha = 10.0
+kg_ha_to_Mg_ha = 0.001
+g_m2_to_Mg_ha = 0.01
+leachn_in2on_ef = 0.0075   # kg N2O-N/kg N leached
